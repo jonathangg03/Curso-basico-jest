574536e81ccb0402fac7c0c1d1c9387dda7df0f8
Jonathan García <jona03g97@gmail.com>
2020-11-30 22:40:31 -0600

    Excepciones Snapshot

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,4 +5,24 @@ describe('Es hora de las instantaneas', () => {
     test('Snapshoot', () => {
         expect(getCharacter(rick)).toMatchSnapshot();
     });
+
+    test('Siempre fallará la instantanea', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() * 20)//Valores aleatorios del 0 al 19
+        }
+        expect(user).toMatchSnapshot();
+    });
+
+
+    test('Tenemos una excepción', () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),//Valores aleatorios del 0 al 19
+            name: 'Jonathan Garcia'
+        }
+
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number)
+        })
+    });
 });
